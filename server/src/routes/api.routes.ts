15d605590f8931579c568070ab25e5f4a079fbb2
Claudivan Filho <j.claudivan.filho@gmail.com>
2022-02-04 09:11:32 -0300

    fix: server problems

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -5,9 +5,9 @@ const apiRouter = Router();
 
 apiRouter.get("/phoneword", (req, res) => {
   if (!req.query.number || isNaN(Number(req.query.number))) {
-    res.status(500).json({ message: "Invalid query string {number}" });
+    res.status(400).json({ message: "Invalid query string {number}" });
   } else {
-    res.json(getT9ExpansionsOfNumber(`${req.query.number}`));
+    res.json(getT9ExpansionsOfNumber(req.query.number as string));
   }
 });
 
