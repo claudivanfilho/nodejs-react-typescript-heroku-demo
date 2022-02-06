f2631a1836db92da9e80c9f062cd9f3b3b596c7b
Claudivan Filho <j.claudivan.filho@gmail.com>
2022-02-06 10:58:30 -0300

    update: refactor package.json

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,12 @@ const app = express();
 
 // Declare the path to frontend's static assets
 console.log(process.env.NODE_ENV);
-app.use(express.static(resolve("..", "front/build")));
+app.use(express.static(resolve("..", "front", "build")));
 app.use(routes);
 
 app.get("*", (_, response) => {
   // response.send("ok");
-  response.sendFile(resolve("..", "front/build", "index.html"));
+  response.sendFile(resolve("..", "front", "build", "index.html"));
 });
 
 export default app;
